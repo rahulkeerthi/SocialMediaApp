4d4ca2f64308c3df45caa064d36561687a8680c3
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-09-14 18:22:39 +0100

    react router with links

--- app/Main.js
+++ app/Main.js
@@ -1,17 +1,30 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import HomeGuest from "./components/HomeGuest"
+import About from "./components/About"
+import Terms from "./components/Terms"
 
 function ExampleComponent() {
 	return (
-		<>
+		<BrowserRouter>
 			<Header />
-			<HomeGuest />
+			<Switch>
+				<Route path='/' exact>
+					<HomeGuest />
+				</Route>
+				<Route path='/about-us' exact>
+					<About />
+				</Route>
+				<Route path='/terms' exact>
+					<Terms />
+				</Route>
+			</Switch>
 			<Footer />
-		</>
+		</BrowserRouter>
 	)
 }
 
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,24 +1,27 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Footer() {
 	return (
 		<footer className='border-top text-center small text-muted py-3'>
 			<p>
-				<a href='/' className='mx-1'>
+				<Link to='/' className='mx-1'>
 					Home
-				</a>
-				<a className='mx-1' href='/about-us'>
+				</Link>
+				|
+				<Link className='mx-1' to='/about-us'>
 					About Us
-				</a>
-				<a className='mx-1' href='/terms'>
+				</Link>
+				|
+				<Link className='mx-1' to='/terms'>
 					Terms
-				</a>
+				</Link>
 			</p>
 			<p className='m-0'>
 				Copyright &copy; 2020
-				<a href='/' className='text-muted'>
+				<Link to='/' className='text-muted'>
 					ComplexApp
-				</a>
+				</Link>
 				. All rights reserved.
 			</p>
 		</footer>
