ed7edc9ae947df6a14da7855a9c1346adfeb5bd0
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-09-21 13:16:15 +0100

    token refresh mechanism

--- app/Main.js
+++ app/Main.js
@@ -86,6 +86,25 @@ function App() {
 		}
 	}, [state.loggedIn])
 
+	useEffect(() => {
+		if (state.loggedIn) {
+			const ourRequest = Axios.CancelToken.source()
+			async function checkToken() {
+				try {
+					const response = await Axios.post("/checkToken", { token: state.user.token }, { cancelToken: ourRequest.token })
+					if (!response.data) {
+						dispatch({ type: "logout" })
+						dispatch({ type: "flashMessage", value: "Your session has expired. Please login again." })
+					}
+				} catch (e) {
+					console.log(e.message)
+				}
+			}
+			checkToken()
+			return () => ourRequest.cancel()
+		}
+	}, [])
+
 	return (
 		<StateContext.Provider value={state}>
 			<DispatchContext.Provider value={dispatch}>
